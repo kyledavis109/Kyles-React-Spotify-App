aeec4e45bad0cffc384e370a41e2d5c9f6e266c2
Kyle Davis <kyledavis109@gmail.com>
2022-01-25 16:22:42 -0600

    Deleted input box from header.

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,6 @@ function Header() {
     return (
         <div className='topnav'>
             <a className='active' href='/'>Home</a>
-            <input type="text" placeholder="Search.."></input>
         </div>
     );
 };
