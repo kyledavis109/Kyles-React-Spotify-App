537837e3685d6eca8862335f33301bf2f745854b
Kyle Davis <kyledavis109@gmail.com>
2022-01-26 14:57:10 -0600

    Created new footer component with new styling.

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,9 +3,47 @@ import './Styles/Footer.css';
 function Footer() {
 
     return (
-        <div className='footer'>
-            <a href='https://www.spotify.com/us/' target='_blank' rel='noreferrer'>Spotify</a>
+        <div>
+<footer className="footer-distributed">
+
+			<div className="footer-left">
+
+				<h3>Kyle<span>Davis</span></h3>
+                <br></br>
+
+				<p className="footer-company-name">Kyle Davis © 2022</p>
+			</div>
+
+			<div className="footer-center">
+
+				<div>
+					<i className="fa fa-map-marker"></i>
+					<p><span>514 Quail Crest Dr.</span> Collierville, TN</p>
+				</div>
+
+				<div>
+					<i className="fa fa-phone"></i>
+					<p>(901) 414-6095</p>
+				</div>
+
+				<div>
+					<i className="fa fa-envelope"></i>
+					<p><a href="mailto:support@company.com">kyledavis109@gmail.com</a></p>
+				</div>
+
+			</div>
+
+			<div className="footer-right">
+
+				<p className="footer-company-about">
+					<span>About Me:</span>
+					I am a junior dev with a passion for coding and music. This website was created from scratch.
+				</p>
+			</div>
+
+		</footer>
         </div>
+        
     );
 };
 
