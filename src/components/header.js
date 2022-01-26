cb9ad395e1bfd5c02bed4960527e2b8fca06dce9
Kyle Davis <kyledavis109@gmail.com>
2022-01-26 14:57:31 -0600

    Created a new header component with new styling.

--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,18 @@ import './Styles/Header.css';
 function Header() {
 
     return (
-        <div className='topnav'>
-            <a className='active' href='/'>Home</a>
+        <div>
+            <br></br>
+            <div class="container">
+	        <div class="header-bar">
+		    <h1 class="logo">Kyle's Music Website!!!</h1>
+		    <div class="slider-menu">
+                <a href='/' >Home</a>
+                <a href='https://github.com/kyledavis109' target='_blank' rel='noreferrer'>GitHub</a>
+                <a href='https://www.spotify.com/us/' target='_blank' rel='noreferrer'>Spotify</a>
+		    </div>
+	        </div>
+            </div>
         </div>
     );
 };
