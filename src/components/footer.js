090f396fcd85c91cb70f51d9982f243d08a5ee8c
Kyle Davis <kyledavis109@gmail.com>
2022-01-27 13:12:54 -0600

    Fixed indentation.

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,43 +2,43 @@ import './Styles/Footer.css';
 
 function Footer() {
     return (
-    <div>
-        <footer className="footer-distributed">
-        
-            <div className="footer-left">
-                <h3>Kyle<span>Davis</span></h3>
-                <br></br>
-                <p className="footer-company-name">Kyle Davis © 2022</p>
-            </div>
-
-            <div className="footer-center">
-
-                <div>
-                    <i className="fa fa-map-marker"></i>
-                    <p><span>514 Quail Crest Dr.</span> Collierville, TN</p>
+        <div>
+            <footer className="footer-distributed">
+            
+                <div className="footer-left">
+                    <h3>Kyle<span>Davis</span></h3>
+                    <br></br>
+                    <p className="footer-company-name">Kyle Davis © 2022</p>
                 </div>
 
-                <div>
-                    <i className="fa fa-phone"></i>
-                    <p>(901) 414-6095</p>
+                <div className="footer-center">
+
+                    <div>
+                        <i className="fa fa-map-marker"></i>
+                        <p><span>514 Quail Crest Dr.</span> Collierville, TN</p>
+                    </div>
+
+                    <div>
+                        <i className="fa fa-phone"></i>
+                        <p>(901) 414-6095</p>
+                    </div>
+
+                    <div>
+                        <i className="fa fa-envelope"></i>
+                        <p><a href="mailto:support@company.com">kyledavis109@gmail.com</a></p>
+                    </div>
+
                 </div>
 
-                <div>
-                    <i className="fa fa-envelope"></i>
-                    <p><a href="mailto:support@company.com">kyledavis109@gmail.com</a></p>
+                <div className="footer-right">
+                    <p className="footer-company-about">
+                        <span>About Me:</span>
+                        I am a junior dev with a passion for coding and music. This website was created from scratch.
+                    </p>
                 </div>
 
-            </div>
-
-            <div className="footer-right">
-                <p className="footer-company-about">
-                    <span>About Me:</span>
-                    I am a junior dev with a passion for coding and music. This website was created from scratch.
-                </p>
-            </div>
-
-        </footer>
-    </div>
+            </footer>
+        </div>
     );
 };
 
--- src/components/header.js
+++ src/components/header.js
@@ -6,14 +6,14 @@ function Header() {
         <div>
             <br></br>
             <div class="container">
-	        <div class="header-bar">
-		    <h1 class="logo">Kyle's Music Website!!!</h1>
-		    <div class="slider-menu">
-                <a href='/' >Home</a>
-                <a href='https://github.com/kyledavis109' target='_blank' rel='noreferrer'>GitHub</a>
-                <a href='https://www.spotify.com/us/' target='_blank' rel='noreferrer'>Spotify</a>
-		    </div>
-	        </div>
+                <div class="header-bar">
+                    <h1 class="logo">Kyle's Music Website!!!</h1>
+                    <div class="slider-menu">
+                        <a href='/' >Home</a>
+                        <a href='https://github.com/kyledavis109' target='_blank' rel='noreferrer'>GitHub</a>
+                        <a href='https://www.spotify.com/us/' target='_blank' rel='noreferrer'>Spotify</a>
+                    </div>
+                </div>
             </div>
         </div>
     );
