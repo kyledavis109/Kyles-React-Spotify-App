8e53849f7d1998d81b73224d6383e11bb2c057cd
Kyle Davis <kyledavis109@gmail.com>
2022-02-08 15:35:23 -0600

    Fixed syntax error.

--- src/components/header.js
+++ src/components/header.js
@@ -3,9 +3,9 @@ import './Styles/Header.css';
 function Header() {
 
     return (
-            <div class="header-bar">
-                <h1 class="rainbow logo">Kyle's Music Website!!!</h1>
-                <div class="slider-menu">
+            <div className="header-bar">
+                <h1 className="rainbow logo">Kyle's Music Website!!!</h1>
+                <div className="slider-menu">
                     <a href='/' >Home</a>
                     <a href='https://github.com/kyledavis109' target='_blank' rel='noreferrer'>GitHub</a>
                     <a href='https://www.spotify.com/us/' target='_blank' rel='noreferrer'>Spotify</a>
