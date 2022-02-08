d2e42278a755c9763753e283e6d1390de85701c1
Kyle Davis <kyledavis109@gmail.com>
2022-02-08 15:36:20 -0600

    Changed class names.

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,31 +6,31 @@ function Footer() {
         
             <div className="footer-section">
                 <h3>Kyle<span>Davis</span></h3>
-                <p className="footer-company-name">Kyle Davis © 2022</p>
+                <p className="footer-name">Kyle Davis © 2022</p>
             </div>
 
             <div className="footer-section">
 
                 <div>
-                    <i className="fa fa-map-marker"></i>
+                    <i className="location"></i>
                     <p><span>123 Something St.</span> Memphis, TN</p>
                 </div>
 
                 <div>
-                    <i className="fa fa-phone"></i>
+                    <i className="phone"></i>
                     <p>(901) 111-1111</p>
                 </div>
 
                 <div>
-                    <i className="fa fa-envelope"></i>
-                    <p><a href="mailto:support@company.com">kyledavis109@gmail.com</a></p>
+                    <i className="email"></i>
+                    <p>kyledavis109@gmail.com</p>
                 </div>
 
             </div>
 
             <div className="footer-section">
                 <p className="footer-company-about">
-                    <span>About Me:</span>
+                    <span>About Me: </span>
                     I am a junior dev with a passion for coding and music. This website was created from scratch.
                 </p>
             </div>
